1b3717f17f4dbdc22d9a6e16e743b8392a14b527
Nguyen Tinh <nguyenquoctinh0310@gmail.com>
2024-03-08 14:39:04 +0700

    first commit

---
+++ client/src/api/todos.ts
@@ -0,0 +1,26 @@
+import { Todo } from '../@types/todo.type';
+import { axiosInstance } from './axiosInstance';
+
+export const todoApi = {
+  getAll(): Promise<Todo[]> {
+    const url = '/todos';
+    return axiosInstance.get(url);
+  },
+  getById(id: string): Promise<Todo> {
+    const url = `/todos/${id}`;
+    return axiosInstance.get(url);
+  },
+
+  add(data: Partial<Todo>): Promise<Todo> {
+    const url = '/todos';
+    return axiosInstance.post(url, data);
+  },
+  update(data: Partial<Todo>): Promise<Todo> {
+    const url = `/todos/${data.id}`;
+    return axiosInstance.patch(url, data);
+  },
+  remove(id: string): Promise<any> {
+    const url = `/todos/${id}`;
+    return axiosInstance.delete(url);
+  },
+};
